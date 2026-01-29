9fd7c69bdc27397adcb05ea3b3e2fe3e643015e8
Anushri Hegde <anushri.hegde1205@gmail.com>
2026-01-29 21:58:38 +0530

    Update masking.py

--- masking.py
+++ masking.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img=cv.imread('c:\\Users\\anush\\OneDrive\\Documents\\Codes\\OpenCV\\Photos\\image 0.jpeg')
+img=cv.imread('Photos\\image 0.jpeg')
 cv.imshow('Image', img)
 
 blank=np.zeros(img.shape[:2], dtype='uint8')
@@ -15,3 +15,4 @@ cv.imshow('Masked Image', masked)
 
 
 cv.waitKey(0)
+
