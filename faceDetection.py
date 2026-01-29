957b4828a70c702861b95986bd07194f1e029a24
Anushri Hegde <anushri.hegde1205@gmail.com>
2026-01-29 21:59:26 +0530

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img= cv.imread('c:\\Users\\anush\\OneDrive\\Documents\\Codes\\OpenCV\\Photos\\peeta.jpeg')
+img= cv.imread('peeta.jpeg')
 cv.imshow('Image', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -15,6 +15,7 @@ print(f'Number of faces detected: {len(faces_rect)}')
 for (x, y, w, h) in faces_rect:
     cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
 cv.imshow('Detected Faces', img)
+
 
 
 
